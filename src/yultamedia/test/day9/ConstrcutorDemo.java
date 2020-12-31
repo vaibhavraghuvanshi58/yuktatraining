6ddd8b3d9496e4e2d1bf04a19a7f32a124b090fa
techellipticaeducation <techellipticaeducation@gmail.com>
2020-12-31 20:01:19 +0530

    Session 12

---
+++ src/yultamedia/test/day9/ConstrcutorDemo.java
@@ -0,0 +1,34 @@
+package yultamedia.test.day9;
+
+public class ConstrcutorDemo {
+public static void main(String[] args) {
+	H h1 =new H();
+	H h2 =new H(10);
+	
+	
+	// instantiation(plot area) |  constructor
+}
+}
+
+
+class H{
+
+	// non parameterised (default) 
+	H(){
+		System.out.println("default constructor");
+	}
+
+	H(int i){
+		System.out.println("one parameterized constructor");
+	}
+	
+	
+	
+	// method 
+	void call() {
+	System.out.println("i am call method");	
+	}
+	
+	
+	
+}
